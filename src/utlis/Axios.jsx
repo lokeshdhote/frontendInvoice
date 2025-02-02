db7a8cabe2fda9660aba4f404f753aa125e0725f
lokeshdhote <lokeshdhote2004@gmail.com>
2025-02-03 00:36:36 +0530

    new

--- src/utlis/Axios.jsx
+++ src/utlis/Axios.jsx
@@ -1,11 +1,40 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+
+//     // baseURL: "http://localhost:3000", 
+//     baseURL:"https://invoicebackend-1.onrender.com/",   
+   
+//     withCredentials: true,
+//   });
+
+// export default instance;
+
+
 import axios from "axios";
 
+// Create an axios instance
 const instance = axios.create({
 
-    // baseURL: "http://localhost:3000", 
-    baseURL:"https://invoicebackend-1.onrender.com/",   
-   
-    withCredentials: true,
-  });
+  
+  baseURL:"https://invoicebackend-1.onrender.com/",    
+ 
+  withCredentials: true,
+});
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("user");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    // Handle any request errors
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
